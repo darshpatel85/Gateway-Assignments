20ae49d714cc1c47fe02ff903469c6139e456c13
darsh patel <darshspatel85@gmail.com>
2020-12-13 14:41:56 +0530

    folder change

---
+++ SourceControl2/SourceControl2/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using sample.Models;
+namespace sample.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ SourceControl2/SourceControl2/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace sample.Models
+{
+    public class Employee
+    {
+        public int  Id { get; set; }
+        [Required(ErrorMessage ="required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(10,MinimumLength =8)]
+        public string Password { get; set; }
+
+    }
+}
