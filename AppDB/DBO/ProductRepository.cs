127d4ebba51146b489ed5e39abca3235e9e95f47
darsh patel <darshspatel85@gmail.com>
2020-12-26 11:44:08 +0530

    Database done.

---
+++ AppDB/DBO/ProductRepository.cs
@@ -0,0 +1,107 @@
+﻿using AppModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDB.DBO
+{
+    public class ProductRepository
+    {
+        public int AddProduct(ProductModel model)
+        {
+            using (var context = new ProductDBEntities())
+            {
+                Product product = new Product()
+                {
+                    Category = model.Category,
+                    Quantity = model.Quantity,
+                    Price = model.Price,
+                    SDes = model.SDes,
+                    LDes = model.LDes,
+                    ImageURL = model.ImageURL
+                };
+                context.Product.Add(product);
+                context.SaveChanges();
+                return product.Id;
+            }
+
+
+        }
+        public List<ProductModel> GetAllProducts()
+        {
+            using (var context = new ProductDBEntities())
+            {
+                var result = context.Product
+                    .Select(x => new ProductModel()
+                    {
+                        Id = x.Id,
+                        Category = x.Category,
+                        Price = x.Price,
+                        Quantity = x.Quantity,
+                        SDes = x.SDes,
+                        ImageURL = x.ImageURL,
+                        LDes = x.LDes
+                    }).ToList();
+
+                return result;
+            }
+        }
+        public ProductModel GetProduct(int id)
+        {
+            using (var context = new ProductDBEntities())
+            {
+                var result = context.Product.Where(x => x.Id == id)
+                    .Select(x => new ProductModel()
+                    {
+                        Id = x.Id,
+                        Category = x.Category,
+                        SDes = x.SDes,
+                        LDes = x.LDes,
+                        Price = x.Price,
+                        ImageURL = x.ImageURL,
+                        Quantity = x.Quantity
+                    }).FirstOrDefault();
+                return (ProductModel)result;
+            }
+        }
+
+        public bool UpdateProduct(int id, ProductModel product)
+        {
+            using (var Context = new ProductDBEntities())
+            {
+                var res = Context.Product.FirstOrDefault(x => x.Id == id);
+                if (res != null)
+                {
+                    res.Category = product.Category;
+                    res.LDes = product.LDes;
+                    res.Price = product.Price;
+                    res.Quantity = product.Quantity;
+                    res.SDes = product.SDes;
+                    res.ImageURL = product.ImageURL;
+                    Context.SaveChanges();
+                    return true;
+                }
+                return false;
+
+            }
+        }
+
+        public bool DeleteProduct(int id, ProductModel product)
+        {
+            using (var context = new ProductDBEntities())
+            {
+                var res = context.Product.FirstOrDefault(x => x.Id == id);
+                if (res != null)
+                {
+                    context.Product.Remove(res);
+                    context.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
+
--- Product-Management/Controllers/HomeController.cs
+++ Product-Management/Controllers/HomeController.cs
@@ -1,30 +1,97 @@
-﻿using System;
+﻿using AppModel.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using AppDB.DBO;
 namespace Product_Management.Controllers
 {
     public class HomeController : Controller
     {
+        ProductRepository produtctRepository = null;
+        public HomeController()
+        {
+            produtctRepository = new ProductRepository();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProductModel productModel, HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                string ImageName = System.IO.Path.GetFileName(file.FileName);
+                string physicalPath = Server.MapPath("~/Images/" + ImageName);
+                file.SaveAs(physicalPath);
+                productModel.ImageURL = ImageName;
+                if (ModelState.IsValid)
+                    {
+                        int id = produtctRepository.AddProduct(productModel);
+                        if (id > 0)
+                        {
+                            ModelState.Clear();
+                            return RedirectToAction("Index");
+                        }
+                        else
+                        {
+                            ViewBag.status = "Not stored..";
+                        }
+                    }
+                else
+                {
+                    ViewBag.status = "State Invalid";
+                }
+            }
+            else
+            {
+                ViewBag.Status = "file Invalid..";
+            }
+            return View();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var result = produtctRepository.GetAllProducts();
+            return View(result);
         }
 
-        public ActionResult About()
+        public ActionResult Details(int id)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            var result = produtctRepository.GetProduct(id);
+            return View(result);
+        }
+        public ActionResult Edit(int id)
+        {
+            var result = produtctRepository.GetProduct(id);
+            return View(result);
+        }
+        [HttpPost]
+        public ActionResult Edit(ProductModel product)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = produtctRepository.UpdateProduct(product.Id, product);
+            }
+            return RedirectToAction("getAllProducts", "Home");
         }
 
-        public ActionResult Contact()
+        public ActionResult Delete(int id)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            var result = produtctRepository.GetProduct(id);
+            return View(result);
+        }
+        [HttpPost]
+        public ActionResult Delete(ProductModel product)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = produtctRepository.DeleteProduct(product.Id, product);
+            }
+            return RedirectToAction("getAllProducts", "Home");
         }
     }
 }
